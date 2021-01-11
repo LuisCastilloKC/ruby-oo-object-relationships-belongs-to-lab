0b6ae3d7d1352d69f9854599df45b9d184aade25
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2021-01-10 19:26:29 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+    attr_accessor :name
+end
+
+
+# Artist
+#   has a name
+
+# Author
+#   has a name
+
+# Post
+#   has a title
+#   belongs to an author
+#   knows the name of the author it belongs to
+
+# Song
+#   has a title
+#   belongs to an artist
+#   knows the name of the artist it belongs to
+
+# Finished in 0.00242 seconds (files took 0.08874 seconds to load)
+# 8 examples, 0 failures
